6e956c0cd80cbb9f3b23d6e0fb6a0c1b53fea6b8
srishakatux <srishakatux@gmail.com>
2015-07-19 19:48:42 -0400

    added ejs module and twit twitter api to get followers ids

--- twitter.js
+++ twitter.js
@@ -1,14 +1,33 @@
 // Load the http module to create an http server.
 var express = require('express');
 var app = express();
-
 var path = require('path');
+var Twit = require('twit');
 
-//app.use(express.static(__dirname)); // Current directory is root
-app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+//Get this data from your twitter apps dashboard
+var T = new Twit({
+    consumer_key: 'consumer_key',
+    consumer_secret: 'consumer_secret',
+    access_token: 'access_token',
+    access_token_secret: 'access_token_secret'
+});
+
+//
+//  get the list of user id's that follow @srish_aka_tux
+//
+T.get('followers/ids', { screen_name: 'srish_aka_tux' },  function (err, data, response) {
+  console.log(JSON.stringify(data, null, 2));
+})
+
+app.engine('.ejs', require('ejs').__express);
+
+// routing for the homepage
+app.get("/", function(req, res) {
+    res.render('index.ejs', {
+    });
+});
 
 app.listen(8000);
-console.log('Listening on port 8000');
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
