4d643975475d56e783637199e0f875ded1e39a58
srishakatux <srishakatux@gmail.com>
2015-07-20 01:02:08 -0400

    being able to load profile image urls of followers

--- twitter.js
+++ twitter.js
@@ -3,6 +3,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var Twit = require('twit');
+var async = require('async');
+var twitter = require('twitter');
+_ = require('underscore')._;
+
+var followers = [];
+var follower_data = [];
 
 //Get this data from your twitter apps dashboard
 var T = new Twit({
@@ -15,16 +21,55 @@ var T = new Twit({
 //
 //  get the list of user id's that follow @srish_aka_tux
 //
-T.get('followers/ids', { screen_name: 'srish_aka_tux' },  function (err, data, response) {
-  console.log(JSON.stringify(data, null, 2));
-})
+function getTwitterFollowers() {
+    T.get('followers/ids', { screen_name: 'srish_aka_tux' },  function (err, data, response) {
+      followers = data.ids;
+    });
+
+    if(followers.length > 0) {
+        for(var i = 0; i< followers.length; i++) {
+        
+            T.get('users/lookup', { user_id: followers[i] }, function(err, data, response) {
+                follower_data = data;
+                return follower_data;
+            });
+        }
+
+    }
+
+}
+
 
 app.engine('.ejs', require('ejs').__express);
 
 // routing for the homepage
 app.get("/", function(req, res) {
-    res.render('index.ejs', {
+
+    T.get('followers/ids', { screen_name: 'srish_aka_tux', count: 5 },  function (err, data, response) {
+        followers = data.ids;
+
+        async.each(followers, function(follower_id, callback) {
+            
+            T.get('users/lookup', { user_id: follower_id }, function(err, data, response) {
+                follower_data.push(data[0].profile_image_url); 
+            });
+
+            callback(); // show that no errors happened
+
+        }, function(err) {
+            if(err) {
+                console.log("There was an error" + err);
+            } else {
+                console.log("Loop is done");
+
+                res.render('index.ejs', {
+                    follower_data: follower_data,
+                });
+            }
+        });
+    
     });
+
 });
 
 app.listen(8000);
