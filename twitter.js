324b61a578b9445b42ec3623ed638efaee021cbb
srishakatux <srishakatux@gmail.com>
2015-07-20 03:03:00 -0400

    profile images mapped to profile urls

--- twitter.js
+++ twitter.js
@@ -9,6 +9,7 @@ _ = require('underscore')._;
 
 var followers = [];
 var follower_data = [];
+var follower_profile_url = [];
 
 //Get this data from your twitter apps dashboard
 var T = new Twit({
@@ -18,40 +19,24 @@ var T = new Twit({
     access_token_secret: 'access_token_secret'
 });
 
-//
-//  get the list of user id's that follow @srish_aka_tux
-//
-function getTwitterFollowers() {
-    T.get('followers/ids', { screen_name: 'srish_aka_tux' },  function (err, data, response) {
-      followers = data.ids;
-    });
-
-    if(followers.length > 0) {
-        for(var i = 0; i< followers.length; i++) {
-        
-            T.get('users/lookup', { user_id: followers[i] }, function(err, data, response) {
-                follower_data = data;
-                return follower_data;
-            });
-        }
-
-    }
-
-}
-
-
 app.engine('.ejs', require('ejs').__express);
+app.use(express.static(__dirname + '/public'));
 
 // routing for the homepage
 app.get("/", function(req, res) {
 
-    T.get('followers/ids', { screen_name: 'srish_aka_tux', count: 5 },  function (err, data, response) {
+    setTimeout(function() {
+
+    }, 1000); 
+
+    T.get('followers/ids', { screen_name: 'srish_aka_tux', count: 30},  function (err, data, response) {
         followers = data.ids;
 
         async.each(followers, function(follower_id, callback) {
-            
+
             T.get('users/lookup', { user_id: follower_id }, function(err, data, response) {
-                follower_data.push(data[0].profile_image_url); 
+                follower_data.push(data[0].profile_image_url.replace("_normal", "")); 
+                follower_profile_url.push("http://twitter.com/" + data[0].screen_name); 
             });
 
             callback(); // show that no errors happened
@@ -64,6 +49,7 @@ app.get("/", function(req, res) {
 
                 res.render('index.ejs', {
                     follower_data: follower_data,
+                    follower_profile_url: follower_profile_url
                 });
             }
         });
